ee92ec6803f1f5297755a37e1820eb01d33c03cd
Enforsix <christian.gulkoder@gmail.com>
2024-11-22 16:11:17 +0100

    Ferdig

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function unitConverter(even) {
     even.preventDefault ();
 
+//vi henter ut informajon fra HTML
     const unitInput = parseFloat(document.querySelector("#unitInput").value);
     const unitFrom = document.querySelector("#unitFrom").value;
     const unitTo = document.querySelector("#unitTo").value;
 
     let displayResult = document.querySelector("#displayResult");
 
-
+//MM som utgangspunkt
     if (unitFrom === "ML" && unitTo === "CL") {
      displayResult.textContent = unitInput / 10; 
     } else if (unitFrom === "ML" && unitTo === "DL") {
@@ -17,8 +18,7 @@ function unitConverter(even) {
     }else if (unitFrom === "ML" && unitTo === "M3") {
      displayResult.textContent = unitInput / 1000000;
 
-
-
+//CL som utgangspunkt
     } else if (unitFrom === "CL" && unitTo === "ML") {
      displayResult.textContent = unitInput * 10;
     }else if (unitFrom === "CL" && unitTo === "DL") {
@@ -28,9 +28,7 @@ function unitConverter(even) {
     }else if (unitFrom === "CL" && unitTo === "M3") {
      displayResult.textContent = unitInput / 100000;
 
-
-
-
+//DL som utgangspunkt
     }else if (unitFrom === "DL" && unitTo === "ML") {
      displayResult.textContent = unitInput * 100;   
     }else if (unitFrom === "DL" && unitTo === "CL") {
@@ -39,23 +37,18 @@ function unitConverter(even) {
      displayResult.textContent = unitInput / 10;
     }else if (unitFrom === "DL" && unitTo === "M3") {
      displayResult.textContent = unitInput / 10000;
-    } 
 
-
-
-
-    else if (unitFrom === "L" && unitTo === "ML") {
+//L som utgangspunkt
+    }else if (unitFrom === "L" && unitTo === "ML") {
     displayResult.textContent = unitInput * 1000;   
-   }else if (unitFrom === "L" && unitTo === "CL") {
+    }else if (unitFrom === "L" && unitTo === "CL") {
     displayResult.textContent = unitInput * 100;   
-   }else if (unitFrom === "L" && unitTo === "DL") {
+    }else if (unitFrom === "L" && unitTo === "DL") {
     displayResult.textContent = unitInput * 10;
-   }else if (unitFrom === "L" && unitTo === "M3") {
-    displayResult.textContent = unitInput /1000;
+    }else if (unitFrom === "L" && unitTo === "M3") {
+    displayResult.textContent = unitInput /1000; 
 
-
-    
-
+//M3 som utgangspunkt
    }else if (unitFrom === "M3" && unitTo === "ML") {
    displayResult.textContent = unitInput * 1000000;   
    }else if (unitFrom === "M3" && unitTo === "CL") {
